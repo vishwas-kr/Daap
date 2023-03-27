a8078027048611b324556c959a6f1881c228af00
kushagra-kumarr <99645798+kushagra-kumarr@users.noreply.github.com>
2023-03-27 11:57:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:robbin_hood_google_soln/views/splash/splash.dart';
+import 'surplus_food_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'robinhood',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
