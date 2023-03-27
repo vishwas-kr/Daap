2ba215660cc1474feedc04eed326c2f54738c916
kushagra-kumarr <99645798+kushagra-kumarr@users.noreply.github.com>
2023-03-27 11:58:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:robbin_hood_google_soln/views/splash/splash.dart';
+import 'surplus_food_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'robinhood',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
 
