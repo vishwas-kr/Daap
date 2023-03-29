30a6eefb73cb61d517ed9219d6034f6fc82e2ca3
kushagra-kumarr <99645798+kushagra-kumarr@users.noreply.github.com>
2023-03-29 10:39:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:robbin_hood_google_soln/views/splash/splash.dart';
-import 'surplus_food_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'robinhood',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme:
+                GoogleFonts.openSansTextTheme(Theme.of(context).textTheme),
+          ),
+          home: child,
+        );
+      },
+      child: const SplashScreen(),
     );
   }
 }
-
